9b2e2e37ecd033a2133b44d2a8c1c34b76596e1b
celoselado <malexandresantos@hotmail.com>
2025-03-14 21:43:35 -0300

    Constantes, variáveis, operadores e estrutura

---
+++ NanoCourse - Learn to Program/Main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+char msg[9] = "Ola Mundo";
+
+#define PI 3.14;
+
+int main() {
+
+    double i = 0;
+    
+    printf(msg);
+    i = 1*PI;
+    printf(i);
+    return 0;
+
+    
+}
