be1732ba25c94eb47fcccc9a9b299dcbb44c10c0
celoselado <malexandresantos@hotmail.com>
2025-03-17 15:38:32 -0300

    Operadores condicionais

--- NanoCourse - Learn to Program/Main.c
+++ NanoCourse - Learn to Program/Main.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
-char msg[9] = "Ola Mundo";
-
-#define PI 3.14;
-
 int main() {
 
-    double i = 0;
-    
-    printf(msg);
-    i = 1*PI;
-    printf(i);
-    return 0;
+    float nota_final, nota1, nota2;
 
-    
+    printf("Entre com o valor da nota 1: ");
+    scanf("%f", &nota1);
+
+    printf("Entre com o valor da nota 2: ");
+    scanf("%f", &nota2);
+
+    nota_final = (nota1 > nota2) ? nota1:nota2;
+
+    printf("O valor da nota final é %f", nota_final);
+
+
+    return 0;
 }
